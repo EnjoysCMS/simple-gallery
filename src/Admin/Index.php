e4b09ae7b7c0dd53bea4d84e863eebc44a76a52d
enjoys <deadly.info@gmail.com>
2021-08-29 21:22:20 +0300

    added UPLOAD_URL and view list images

--- src/Admin/Index.php
+++ src/Admin/Index.php
@@ -10,20 +10,25 @@ use App\Module\Admin\Core\ModelInterface;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\Persistence\ObjectRepository;
+use EnjoysCMS\Core\Components\Modules\ModuleConfig;
+use EnjoysCMS\Module\SimpleGallery\Config;
 use EnjoysCMS\Module\SimpleGallery\Entities\Image;
 
 final class Index implements ModelInterface
 {
     private ObjectRepository|EntityRepository $repository;
+    private ModuleConfig $config;
 
     public function __construct(private EntityManager $em)
-{
-    $this->repository = $this->em->getRepository(Image::class);
-}
+    {
+        $this->repository = $this->em->getRepository(Image::class);
+        $this->config = Config::getConfig();
+    }
 
     public function getContext(): array
     {
         return [
+            'config' => $this->config,
             'images' => $this->repository->findAll()
         ];
     }
