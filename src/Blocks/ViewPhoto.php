5dbdd1a82a84a01ed6a7a3f50721ebb5f815c1e3
Enjoys <deadly.info@gmail.com>
2021-09-01 10:09:46 +0300

    added cache result

--- src/Blocks/ViewPhoto.php
+++ src/Blocks/ViewPhoto.php
@@ -8,11 +8,17 @@ namespace EnjoysCMS\Module\SimpleGallery\Blocks;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Query\Expr\Join;
-use EnjoysCMS\Core\Components\Blocks\AbstractBlock;
-use EnjoysCMS\Module\SimpleGallery\Entities\Image;
-use Twig\Environment;
 use DoctrineExtensions\Query\Mysql\Rand;
+use Enjoys\SimpleCache\CacheException;
+use Enjoys\SimpleCache\Cacher\FileCache;
+use EnjoysCMS\Core\Components\Blocks\AbstractBlock;
+use EnjoysCMS\Module\SimpleGallery\Config;
+use EnjoysCMS\Module\SimpleGallery\Entities\Image;
+use Psr\SimpleCache\InvalidArgumentException;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ViewPhoto extends AbstractBlock
 {
@@ -22,6 +28,13 @@ class ViewPhoto extends AbstractBlock
         return __DIR__ . '/../../blocks.yml';
     }
 
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws CacheException
+     * @throws LoaderError
+     * @throws InvalidArgumentException
+     */
     public function view()
     {
         /** @var EntityManager $em */
@@ -31,11 +44,19 @@ class ViewPhoto extends AbstractBlock
         /** @var Environment $twig */
         $twig = $this->container->get(Environment::class);
 
+        $cacher = new FileCache(['path' => $_ENV['TEMP_DIR'] . '/cache/blocks']);
+
+        if (null === $result = $cacher->get($this->block->getAlias())) {
+            $result = $this->getImages($em->getRepository(Image::class));
+            $cacher->set($this->block->getAlias(), $result, (int)$this->getOption('cacheTime', 0));
+        }
+
         return $twig->render(
             (string)$this->getOption('template'),
             [
-                'images' => $this->getImages($em->getRepository(Image::class)),
-                'options' => $this->getOptions()
+                'images' => $result,
+                'options' => $this->getOptions(),
+                'config' => Config::getConfig()->getAll()
             ]
         );
     }
@@ -44,13 +65,11 @@ class ViewPhoto extends AbstractBlock
     {
         $qb = $repository->createQueryBuilder('i');
 
-
         $qb = match ($this->getOption('orderItems')) {
             default => $qb->orderBy('i.id', 'ASC'),
             'desc' => $qb->orderBy('i.id', 'DESC'),
             'random' => $qb->orderBy('RAND()'),
         };
-
 
         return $qb->getQuery()
             ->setMaxResults(
