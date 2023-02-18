a1e47437038508eb3e9da49c84eb7378f927f6e7
enjoys <deadly.info@gmail.com>
2023-02-18 20:13:22 +0300

    replace Enjoys\SimpleCache on Symfony\Cache

--- src/Blocks/ViewPhoto.php
+++ src/Blocks/ViewPhoto.php
@@ -9,16 +9,16 @@ namespace EnjoysCMS\Module\SimpleGallery\Blocks;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 use DoctrineExtensions\Query\Mysql\Rand;
-use Enjoys\SimpleCache\CacheException;
-use Enjoys\SimpleCache\Cacher\FileCache;
 use EnjoysCMS\Core\Components\Blocks\AbstractBlock;
 use EnjoysCMS\Core\Entities\Block as Entity;
 use EnjoysCMS\Module\SimpleGallery\Config;
 use EnjoysCMS\Module\SimpleGallery\Entities\Image;
+use Psr\Cache\InvalidArgumentException;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Container\NotFoundExceptionInterface;
-use Psr\SimpleCache\InvalidArgumentException;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Contracts\Cache\ItemInterface;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -26,10 +26,10 @@ use Twig\Error\SyntaxError;
 
 class ViewPhoto extends AbstractBlock
 {
-public function __construct(private ContainerInterface $container, Entity $block)
-{
-    parent::__construct($block);
-}
+    public function __construct(private ContainerInterface $container, Entity $block)
+    {
+        parent::__construct($block);
+    }
 
     public static function getBlockDefinitionFile(): string
     {
@@ -37,16 +37,15 @@ public function __construct(private ContainerInterface $container, Entity $block
     }
 
     /**
-     * @throws CacheException
-     * @throws InvalidArgumentException
+
      * @throws LoaderError
      * @throws RuntimeError
      * @throws SyntaxError
-     * @throws \Enjoys\SimpleCache\InvalidArgumentException
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
+     * @throws InvalidArgumentException
      */
-    public function view()
+    public function view(): string
     {
         /** @var EntityManager $em */
         $em = $this->container->get(EntityManager::class);
@@ -55,14 +54,13 @@ public function __construct(private ContainerInterface $container, Entity $block
         /** @var Environment $twig */
         $twig = $this->container->get(Environment::class);
 
-        $cacher = new FileCache(['path' => $_ENV['TEMP_DIR'] . '/cache/blocks']);
+        $cache = new FilesystemAdapter('blocks', directory: $_ENV['TEMP_DIR'] . '/cache');
 
-        $cacheId = md5($this->block->getAlias().json_encode($this->getOptions()));
-
-        if (null === $result = $cacher->get($cacheId)) {
-            $result = $this->getImages($em->getRepository(Image::class));
-            $cacher->set($cacheId, $result, (int)$this->getOption('cacheTime', 0));
-        }
+        $cacheId = md5($this->block->getAlias() . json_encode($this->getOptions()));
+        $result = $cache->get($cacheId, function (ItemInterface $item) use ($em) {
+            $item->expiresAfter((int)$this->getOption('cacheTime', 0));
+            return $this->getImages($em->getRepository(Image::class));
+        });
 
         return $twig->render(
             (string)$this->getOption('template'),
@@ -87,7 +85,8 @@ public function __construct(private ContainerInterface $container, Entity $block
         return $qb->getQuery()
             ->setMaxResults(
                 (int)$this->getOption('cntItems', 10)
-            )->getResult();
+            )->getResult()
+        ;
     }
 
 
