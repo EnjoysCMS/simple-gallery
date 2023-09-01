1a12f23deb67fdc0bd7ab8775828a1fc6a75cfd9
enjoys <deadly.info@gmail.com>
2023-09-01 23:29:30 +0300

    improved composer scripts

---
+++ scripts/AssetsInstallCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EnjoysCMS\Module\SimpleGallery\Composer\Scripts;
+
+use EnjoysCMS\Core\Console\Command\AbstractAssetsInstallCommand;
+
+class AssetsInstallCommand extends AbstractAssetsInstallCommand
+{
+    protected string $cwd = __DIR__ . '/..';
+}
